e5022c53e9195035accd41caf4f949580b55a664
Margaret <rixten.6@gmail.com>
2024-04-29 15:37:42 +0300

    add mocks

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -7,14 +7,14 @@ export const socials = [
   "dribbble",
 ];
 export const skills = [
-  ["Html", "90%"],
-  ["CSS", "85%"],
-  ["Js", "80%"],
-  ["PHP", "75%"],
-  ["WordPress", "85%"],
+  ["Html5", "90%"],
+  ["CSS3", "85%"],
+  ["JavaScript", "80%"],
+  ["TypeScript", "75%"],
+  ["React", "85%"],
 ];
 export const languages = [
-  ["Bangle", "100%"],
+  ["Russian", "100%"],
   ["English", "80%"],
   ["Spanish", "60%"],
 ];
@@ -23,4 +23,102 @@ export const extraSkills = [
   "Stylus, Sass, Less",
   "Gulp, Webpack, Grunt",
   "GIT Knowledge",
+];
+
+export const education = [
+  {
+    key: 0,
+    date: "Jan 1016 - Dec 2021",
+    placeName: "Lead Web Designer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor." +
+      "Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce " +
+      "tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    jobTitle: "Student",
+    title: "Certificate of web training",
+  },
+  {
+    key: 1,
+    date: "Jan 1016 - Dec 2021",
+    placeName: "Junior Web Designer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor." +
+      "Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce " +
+      "tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    jobTitle: "Student",
+    title: "Certificate of web training",
+  },
+  {
+    key: 2,
+    date: "Jan 1016 - Dec 2021",
+    placeName: "Senior Web Designer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor." +
+      "Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce " +
+      "tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    jobTitle: "Student",
+    title: "Certificate of web training",
+  },
+];
+export const workHistory = [
+  {
+    key: 0,
+    date: "Jan 1016 - Dec 2021",
+    placeName: "University of Toronto",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor." +
+      "Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce " +
+      "tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    jobTitle: "Student",
+    title: "Certificate of web training",
+  },
+  {
+    key: 1,
+    date: "Jan 1016 - Dec 2021",
+    placeName: "Programming Course",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor." +
+      "Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce " +
+      "tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    jobTitle: "Student",
+    title: "Certificate of web training",
+  },
+  {
+    key: 2,
+    date: "Jan 1016 - Dec 2021",
+    placeName: "Web developer courses",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor." +
+      "Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce " +
+      "tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    jobTitle: "Student",
+    title: "Certificate of web training",
+  },
+];
+
+export const menuItems = [
+  "home",
+  "file-code",
+  "user-icon",
+  "portfolio",
+  "blog",
+  "contact",
+];
+
+export const placeOfResidence = [
+  ["Country:", "Bangladesh"],
+  ["City:", "Bangladesh"],
+  ["Streat:", "35 vhatara, Badda"],
+];
+
+export const contactInfo = [
+  ["Email:", "youremail@gmail.com"],
+  ["Skype:", "@yourusername"],
+  ["Telegram:", "@yourusername"],
 ];
