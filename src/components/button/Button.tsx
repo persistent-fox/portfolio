b2829d8d6236a2d4ba745ad1312907c0b3940e9d
Margaret <rixten.6@gmail.com>
2024-07-03 18:52:09 +0300

    do refactor

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,91 +1,86 @@
-import React from "react";
-import { Icon } from "../icon/Icon";
-import styled, { css } from "styled-components";
+import React from 'react';
+import { Icon } from '../icon/Icon';
+import styled, { css } from 'styled-components';
 
 interface IButtonProps {
-  primary?: boolean;
-  children: React.ReactNode;
-  width?: string;
-  height?: string;
-  viewBox?: string;
-  iconId?: string;
-  color?: string;
-  fontWeight?: string;
-  type?: "submit" | "button" | "reset" | undefined;
+	primary?: boolean;
+	children: React.ReactNode;
+	width?: string;
+	height?: string;
+	viewBox?: string;
+	iconId?: string;
+	color?: string;
+	fontWeight?: string;
+	type?: 'submit' | 'button' | 'reset' | undefined;
 }
 
 export const Button = ({
-  children,
-  primary,
-  width,
-  height,
-  viewBox,
-  iconId,
-  color,
-  fontWeight,
-  type = "submit",
+	children,
+	primary,
+	width,
+	height,
+	viewBox,
+	iconId,
+	color,
+	fontWeight,
+	type = 'submit',
 }: IButtonProps) => {
-  return (
-    <StyledButton
-      type={type}
-      fontWeight={fontWeight}
-      color={color}
-      primary={primary}
-    >
-      {children}
-      {iconId && (
-        <Icon width={width} height={height} viewBox={viewBox} iconId={iconId} />
-      )}
-    </StyledButton>
-  );
+	return (
+		<StyledButton type={type} fontWeight={fontWeight} color={color} primary={primary}>
+			{children}
+			{iconId && <Icon width={width} height={height} viewBox={viewBox} iconId={iconId} />}
+		</StyledButton>
+	);
 };
 
 type StyledButtonProps = {
-  primary?: boolean;
-  fontSize?: string;
-  color?: string;
-  fontWeight?: string;
-  type?: string;
+	primary?: boolean;
+	fontSize?: string;
+	color?: string;
+	fontWeight?: string;
+	type?: string;
 };
 
 export const StyledButton = styled.button<StyledButtonProps>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  font-size: 14px;
-  font-weight: 600;
-  padding: 10px 30px;
-  color: ${(props) => props.color || props.theme.colors.tertiary};
-  background-image: linear-gradient(
-    to right,
-    ${(props) => props.theme.colors.secondary},
-    ${(props) => props.theme.colors.accent}
-  );
-  border: none;
-  cursor: pointer;
-  transition: 0.5s all;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
+	font-size: 14px;
+	font-weight: 600;
+	padding: 10px 30px;
+	color: ${props => props.color || props.theme.colors.tertiary};
+	background-image: linear-gradient(
+		to right,
+		${props => props.theme.colors.secondary},
+		${props => props.theme.colors.accent}
+	);
+	background-size: 200%;
+	border: none;
+	cursor: pointer;
+	transition: 0.5s all;
 
-  &:active {
-    transform: scale(0.95);
-  }
-  &:hover {
-    background-image: linear-gradient(
-      to right,
-      ${(props) => props.theme.colors.accent},
-      ${(props) => props.theme.colors.secondary}
-    );
-  }
+	&:active {
+		transform: scale(0.95);
+	}
+	&:hover {
+		/* background-image: linear-gradient(
+			to right,
+			${props => props.theme.colors.accent},
+			${props => props.theme.colors.secondary}
+		); */
+		background-position: right;
+	}
 
-  // primary button
-  ${(props) =>
-    props.primary &&
-    css<StyledButtonProps>`
-      color: ${(props) => props.color || props.theme.colors.tertiary};
-      font-size: ${(props) => props.fontSize || "16px"};
-      font-weight: ${(props) => props.fontWeight || "500"};
-      padding: 16px 32px;
-      border-radius: 5px;
-      text-transform: uppercase;
-    `}
+	// primary button
+	${props =>
+		props.primary &&
+		css<StyledButtonProps>`
+			color: ${props => props.color || props.theme.colors.tertiary};
+			font-size: ${props => props.fontSize || '16px'};
+			font-weight: ${props => props.fontWeight || '500'};
+			padding: 16px 32px;
+			border-radius: 5px;
+			text-transform: uppercase;
+		`}
 `;
--- src/layout/sections/portfolio/Portfolio.tsx
+++ src/layout/sections/portfolio/Portfolio.tsx
@@ -1,63 +1,58 @@
-import { TitleSection } from "../../../components/TitleSection";
-import { Project } from "./project/Project";
-import srcImg from "./../../../assets/images/projects/proj-1.webp";
-import { FlexWrapper } from "../../../components/FlexWrapper";
-import { S } from "./Portfolio_Styles";
-import { ProjectsData, tabs } from "../../../mock/data";
-import React, { useEffect, useState } from "react";
-import { TabMenu } from "../../../components/tab-menu/TabMenu";
-import "react-alice-carousel/lib/alice-carousel.css";
-import { AnimatePresence, motion } from "framer-motion";
+import { TitleSection } from '../../../components/TitleSection';
+import { Project } from './project/Project';
+import srcImg from './../../../assets/images/projects/proj-1.webp';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import { S } from './Portfolio_Styles';
+import { ProjectsData, tabs } from '../../../mock/data';
+import React, { useEffect, useState } from 'react';
+import { TabMenu } from '../../../components/tab-menu/TabMenu';
+import { AnimatePresence, motion } from 'framer-motion';
 
-export const Portfolio: React.FC<IPortfolioProps> = () => {
-  const [filter, setFilter] = useState("all");
-  const [filtredProjects, setFiltredProjects] = useState(ProjectsData);
+export const Portfolio = () => {
+	const [filter, setFilter] = useState('all');
+	const [filtredProjects, setFiltredProjects] = useState(ProjectsData);
 
-  const onClickTabHandler = (value: string) => {
-    setFilter(value);
-  };
+	const onClickTabHandler = (value: string) => {
+		setFilter(value);
+	};
 
-  useEffect(() => {
-    setFiltredProjects(ProjectsData.filter((item) => item.status === filter));
-    if (filter === "all") {
-      setFiltredProjects(ProjectsData);
-    }
-  }, [filter]);
+	useEffect(() => {
+		setFiltredProjects(ProjectsData.filter(item => item.status === filter));
+		if (filter === 'all') {
+			setFiltredProjects(ProjectsData);
+		}
+	}, [filter]);
 
-  return (
-    <S.Portfolio>
-      <TitleSection>Portfolio</TitleSection>
-      <p>
-        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequat duis enim velit mollit. lorem ipsum
-      </p>
-      <TabMenu
-        tabs={tabs}
-        onClickTabHandler={onClickTabHandler}
-        filter={filter}
-      />
-      <FlexWrapper justify={"center"} gap={"40px"} wrap={"wrap"}>
-        <AnimatePresence>
-          {filtredProjects.map((item) => (
-            <motion.div
-              layout
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              style={{ width: "45%", minWidth: "380px" }}
-              key={item.id}
-            >
-              <Project
-                key={item.id}
-                title={item.title}
-                description={item.description}
-                srcImg={srcImg}
-                skills={item.skills}
-              />
-            </motion.div>
-          ))}
-        </AnimatePresence>
-      </FlexWrapper>
-    </S.Portfolio>
-  );
+	return (
+		<S.Portfolio>
+			<TitleSection>Portfolio</TitleSection>
+			<p>
+				Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim
+				velit mollit. lorem ipsum
+			</p>
+			<TabMenu tabs={tabs} onClickTabHandler={onClickTabHandler} filter={filter} />
+			<FlexWrapper justify={'center'} gap={'40px'} wrap={'wrap'}>
+				<AnimatePresence>
+					{filtredProjects.map(item => (
+						<motion.div
+							layout
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							style={{ width: '45%', minWidth: '380px' }}
+							key={item.id}
+						>
+							<Project
+								key={item.id}
+								title={item.title}
+								description={item.description}
+								srcImg={srcImg}
+								skills={item.skills}
+							/>
+						</motion.div>
+					))}
+				</AnimatePresence>
+			</FlexWrapper>
+		</S.Portfolio>
+	);
 };
